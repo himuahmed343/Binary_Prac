0309b2827e3b11e7c9d134e414281c9934af7f97
himubhai20 <himuahmed343@gmail.com>
2021-07-07 00:22:53 +0600

    LL- Insert at begining function added

---
+++ Linked List/himu_LL_beginning04.py
@@ -0,0 +1,39 @@
+class Node:
+    
+    def __init__(self, data="Head", next = None):
+        self.data = data
+        self.next = next
+
+
+class LinnkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def insert_begin(self, data):
+        node = Node(data, self.head.next)
+        self.head.next = node
+
+    def print_func(self):
+        if self.head.next == None:
+            print('Empty List')
+            return
+        
+        curr_node = self.head
+        result = ''
+        while curr_node != None:
+            result = result + str(curr_node.data) + '-->'
+            curr_node = curr_node.next
+        print(result)
+
+
+
+
+
+if __name__ == '__main__':
+    llt = LinnkedList()
+    llt.insert_begin(10)
+    llt.insert_begin(20)
+    llt.insert_begin(30)
+    llt.print_func()
+        
+        
---
+++ Linked List/himu_LL_beginning05.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data = "Head", next = None):
+        self.data = data
+        self.next = next
+    
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+    
+
+    def add_at_begin(self, data):
+        node = Node(data, self.head.next)
+        self.head.next = node
+    
+    def print_func(self):
+        if self.head.next == None:
+            print("Empty List")
+            return
+
+        new_node = self.head
+        res_str = ''
+        while new_node != None:
+            res_str = res_str + str(new_node.data) + "==>"
+            new_node = new_node.next
+        print(res_str)
+
+
+ll = LinkedList()
+ll.add_at_begin(10)
+ll.add_at_begin(25)
+ll.add_at_begin(87)
+ll.add_at_begin(65)
+ll.print_func()
+
+        
+
+        
